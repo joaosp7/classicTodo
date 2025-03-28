0205bb4297d674a5f91b258d65d2465bba98e215
João Lucas Rodrigues <joaosp7@gmail.com>
2025-03-28 12:51:50 -0300

    env variables and mongoose connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { env } from './env/env';
 
 @Module({
-  imports: [],
+  imports: [TodoModule, MongooseModule.forRoot(env.CONECTION_STRING)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/env/env.ts
@@ -0,0 +1,17 @@
+import z from 'zod';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const envSchema = z.object({
+  DB_PASSWORD: z.string(),
+  CONECTION_STRING: z.string(),
+});
+
+const envParse = envSchema.safeParse(process.env);
+
+if (!envParse.success) {
+  throw new Error('Invalid Enviromnent Variables! Please try again!');
+}
+
+export const env = envParse.data;
