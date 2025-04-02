db8379bda2017354f4f51cd2bb597707e4e53fe2
João Lucas Rodrigues <joaosp7@gmail.com>
2025-04-02 07:10:18 -0300

    class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   username: string;
+  @IsNotEmpty()
+  @Transform(({ value }) => (value = 'oi'))
   password: string;
 }
